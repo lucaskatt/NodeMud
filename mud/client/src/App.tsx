186d16b7b072076a5652d89ee35737e71ddc2f0e
lucaskatt <lkatt@umich.edu>
2017-11-12 07:58:38 -0600

    basic style and started input handling

--- mud/client/src/App.tsx
+++ mud/client/src/App.tsx
@@ -3,23 +3,84 @@ import * as io from 'socket.io-client';
 import './App.css';
 
 class App extends React.Component {
-  state = {prompt: ''};
+  public state: any;
+
   private socket: any;
+  private inputTextField: HTMLInputElement | null;
+  private textLog: HTMLElement | null;
 
-  componentDidMount() {
-    this.socket = io('http://localhost:3001');
-
-    fetch('/prompt')
-      .then((res: any) => res.text())
-      .then((prompt: string) => this.setState({ prompt }));
+  constructor(props: any) {
+    super(props);
+    this.state = {inputText: '', displayedText: []};
   }
 
-  render() {
+  public componentDidMount() {
+    this.socket = io('http://localhost:3001');
+    if (this.socket) {
+
+    }
+  }
+
+  public render() {
     return (
       <div className="App">
-        {this.state.prompt}
+        <div
+          className="textLog"
+          ref={input => this.textLog = input}
+        >
+          {this.state.displayedText.map((line: string) =>
+            <div className="textDiv">
+              {line}
+            </div>
+          )}
+        </div>
+        <form
+          name="input"
+          id="textForm"
+          onSubmit={(event) => { return this.handleSubmit(event); }}
+        >
+          <input
+            name="textInput"
+            type="text"
+            value={this.state.inputText}
+            id="textInput"
+            onChange={({target}: {target: HTMLInputElement}) => this.handleTextChange(target)}
+            ref={input => this.inputTextField = input}
+          />
+          <input
+            name="submitText"
+            type="submit"
+            id="submitText"
+            value="Send"
+          />
+        </form>
       </div>
     );
+  }
+
+  private handleTextChange(target: HTMLInputElement): void {
+      this.setState({inputText: target.value});
+  }
+
+  private handleSubmit(event: any): boolean {
+    event.preventDefault();
+    if (!this.state.inputText) {
+      return false;
+    }
+    this.addText(this.state.inputText);
+    this.resetInput();
+    return false;
+  }
+
+  private resetInput() {
+    this.state.inputText = '';
+    if (this.inputTextField) {
+      this.inputTextField.value = '';
+    }
+  }
+
+  private addText(text: string) {
+    this.setState({displayedText: [...this.state.displayedText, text]});
   }
 }
 
